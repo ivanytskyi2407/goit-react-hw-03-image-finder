0df17b18345fe161872e985b5d3c49c0017ad9a8
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-05-11 18:54:03 +0300

    +

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -8,6 +8,7 @@ export default class ImageGalleryItem extends Component {
   state = {
     query: null,
     loading: false,
+    error: null,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -19,8 +20,14 @@ export default class ImageGalleryItem extends Component {
       fetch(
         `https://pixabay.com/api/?key=25715337-58cde3c0d1b1902de73779f35&q=${this.props.query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=12`
       )
-        .then(data => data.json())
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error(`Помилка завантаження`));
+        })
         .then(query => this.setState({ query: query.hits }))
+        .catch(error => this.setState({ error }))
         .finally(() => {
           this.setState({ loading: false });
         });
@@ -28,10 +35,11 @@ export default class ImageGalleryItem extends Component {
     console.log(this.state.query);
   }
   render() {
-    const { query } = this.state;
+    const { query, loading, error } = this.state;
     return (
       <>
-        {this.state.loading && <div>LOADING</div>}
+        {loading && <div>LOADING</div>}
+        {error && <h1>{`Нічого немає по запиту ${this.props.query}`}</h1>}
         {query &&
           query.map(({ id, webformatURL, tags }) => {
             return (
