0532787d90198f14500c1ae3e715e86f96253aea
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-05-12 19:19:43 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
+import s from './App.module.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import Searchbar from './Searchbar/Searchbar';
+import { Loader } from './Loader/Loader';
+import API from './API/API';
+import { toast } from 'react-toastify';
 import ImageGallery from './ImageGallery/ImageGallery';
-import s from './App.module.css';
 import Button from './Button/Button';
 
 export default class App extends Component {
   state = {
     query: '',
+    loader: false,
     page: 1,
+    pictures: [],
     loadMore: false,
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({ loader: true });
+
+      API(query, page).then(pictures => {
+        this.setState({ loader: false });
+        if (pictures.length === 0) {
+          return toast.error('Нічого немає');
+        }
+        this.setState(prevState => ({
+          pictures: [...prevState.pictures, ...pictures],
+          loadMore: true,
+        }));
+      });
+    }
+  }
   handlFormSubmit = query => {
     this.setState({ query });
-    setInterval(() => {
-      this.setState({ loadMore: true });
-    }, 1000);
+    this.defaultState();
   };
+
   onClickButton = () => {
     this.setState(prevState => {
       return {
@@ -26,13 +47,18 @@ export default class App extends Component {
       };
     });
   };
+  defaultState = () => {
+    this.setState({ page: 1, pictures: [] });
+  };
 
   render() {
+    const { loader, pictures, loadMore } = this.state;
     return (
       <div className={s.App}>
         <Searchbar onSubmit={this.handlFormSubmit} />
-        <ImageGallery query={this.state.query} page={this.state.page} />
-        {this.state.loadMore && <Button onClickButton={this.onClickButton} />}
+        {loader && <Loader />}
+        <ImageGallery pictures={pictures}></ImageGallery>
+        {loadMore && <Button onClickButton={this.onClickButton} />}
         <ToastContainer autoClose={2000} position="top-center" />
       </div>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,54 +1,13 @@
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import { Component } from 'react';
-import API from '../API/API';
 import Modal from '../Modal/Modal';
-import { Loader } from '../Loader/Loader';
 
 export default class ImageGallery extends Component {
   state = {
-    pictures: [],
-    error: null,
-    status: 'idle',
     showModal: false,
     largeImageURL: null,
   };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevProps.query !== this.props.query ||
-      prevProps.page !== this.props.page
-    ) {
-      this.setState({ status: 'pending' });
-      API(this.props.query, this.props.page)
-        .then(pictures => {
-          if (pictures.length === 0) {
-            return alert('нічого немає');
-          }
-          this.setState(prevState => ({
-            pictures: [...prevState.pictures, ...pictures],
-          }));
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }))
-        .finally(
-          this.setState({
-            status: 'resolved',
-          })
-        );
-    }
-  }
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.setState({ showModal: false });
-    }
-  };
-
   handleModal = largeImageURL => {
     this.setState({
       showModal: !this.state.showModal,
@@ -61,32 +20,32 @@ export default class ImageGallery extends Component {
       this.setState({ showModal: false });
     }
   };
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.setState({ showModal: false });
+    }
+  };
 
   render() {
-    const { pictures, status, showModal } = this.state;
-
-    if (status === 'idle') {
-      return;
-    }
-    if (status === 'pending') {
-      return Loader();
-    }
-
-    if (status === 'rejected') {
-      return <h1>{`Нічого немає по запиту ${this.props.query}`}</h1>;
-    }
-    if (status === 'resolved') {
-      return (
-        <ul className={s.ImageGallery}>
-          <ImageGalleryItem pictures={pictures} showModal={this.handleModal} />
-          {showModal && (
-            <Modal
-              bigPicture={this.state.largeImageURL}
-              closeModal={this.closeModal}
-            />
-          )}
-        </ul>
-      );
-    }
+    return (
+      <ul className={s.ImageGallery}>
+        <ImageGalleryItem
+          pictures={this.props.pictures}
+          showModal={this.handleModal}
+        />
+        {this.state.showModal && (
+          <Modal
+            bigPicture={this.state.largeImageURL}
+            closeModal={this.closeModal}
+          />
+        )}
+      </ul>
+    );
   }
 }
